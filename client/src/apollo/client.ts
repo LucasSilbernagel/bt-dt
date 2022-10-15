83a5ac6b6405a76de7be28f55dabfd989917b49b
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2022-10-15 15:18:12 -0400

    undid server uri change

--- client/src/apollo/client.ts
+++ client/src/apollo/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
-const httpLink = new HttpLink({ uri: process.env.REACT_APP_SERVER_URL })
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
