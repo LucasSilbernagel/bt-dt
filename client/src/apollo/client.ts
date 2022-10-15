fade853423eae98681fbe6869da22879057cde63
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2022-10-15 18:33:57 -0400

    another deployment attempt

--- client/src/apollo/client.ts
+++ client/src/apollo/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_SERVER_URL })
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
